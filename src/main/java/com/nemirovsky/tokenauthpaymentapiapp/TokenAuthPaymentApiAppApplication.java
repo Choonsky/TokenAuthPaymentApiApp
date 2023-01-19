085e2b1d460462bf346927a6e660344e96a6a265
Stanislav Nemirovsky <choonsky@gmail.com>
2023-01-19 13:07:59 +0600

    Initial commit

---
+++ src/main/java/com/nemirovsky/tokenauthpaymentapiapp/TokenAuthPaymentApiAppApplication.java
@@ -0,0 +1,13 @@
+package com.nemirovsky.tokenauthpaymentapiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenAuthPaymentApiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokenAuthPaymentApiAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nemirovsky/tokenauthpaymentapiapp/TokenAuthPaymentApiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nemirovsky.tokenauthpaymentapiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokenAuthPaymentApiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
